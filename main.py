b4bce28c719f619ff835f2089f27b0e62db96fc9
NekoCarreraDesigns <denvermaas@gmail.com>
2022-02-15 14:04:02 -0700

    added movement to game

--- main.py
+++ main.py
@@ -9,10 +9,11 @@ pygame.display.set_caption("Pimpin ain't easy")
 playerImg = pygame.image.load('space.png')
 playerX = 370
 playerY = 480
+playerX_change = 0
 
 
-def player():
-    screen.blit(playerImg, (playerX, playerY))
+def player(x, y):
+    screen.blit(playerImg, (x, y))
 
 
 def draw_window(screen):
@@ -28,5 +29,15 @@ while run:
         if event.type == pygame.QUIT:
             run = False
 
-    player()
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_LEFT:
+                playerX_change = -0.5
+            if event.key == pygame.K_RIGHT:
+                playerX_change = 0.5
+        if event.type == pygame.KEYUP:
+            if event.key == pygame.K_LEFT or event.key == pygame.K_RIGHT:
+                playerX_change = 0
+
+    playerX += playerX_change
+    player(playerX, playerY)
     pygame.display.update()
